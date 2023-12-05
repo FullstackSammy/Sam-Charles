47345094fa67703d3bda5197f7f938b3937b9f27
samzzii <nasstromsamuel@icloud.com>
2023-12-05 15:11:12 +0100

    added templates and views

--- sam_charles/views.py
+++ sam_charles/views.py
@@ -5,3 +5,8 @@ from .models import Song
 # Create your views here.
 def index(request):
     songs = Song.objects.all()
+    context = {
+        'songs': songs,
+    }
+    
+    return(request, 'sam_charles/index.html', context)
