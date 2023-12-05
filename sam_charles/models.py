1f7e21139930198b3acabddadab495d80fc5e8cb
samzzii <nasstromsamuel@icloud.com>
2023-12-05 14:36:04 +0100

    added models for songs for website

--- sam_charles/models.py
+++ sam_charles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Songs(models.Model):
+    GENRES = (
+        ('country', 'Country'),
+        ('pop', 'Pop'),
+        ('folk', 'Folk'),
+    )
+    
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50, choices=GENRES)
+    
+    def __str__(self):
+        return f'Song: {self.title} | Genre: {self.genre}'
